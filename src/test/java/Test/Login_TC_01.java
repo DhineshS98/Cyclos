717d3849f856882f1b68b9bf3062b932853eb05f
DhineshS98 <dhineshkumar1308@gmail.com>
2025-05-30 20:55:01 +0530

    This is first commit

---
+++ src/test/java/Test/Login_TC_01.java
@@ -0,0 +1,17 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class Login_TC_01 extends Loginpage {
+	
+	WebDriver driver;
+	
+	@Test
+	public void Positivelogin()
+	{
+		
+		
+	}
+
+}
